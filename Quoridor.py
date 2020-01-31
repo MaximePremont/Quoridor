55eb94cf1f45814d8d9db7d774f4c76a86c5ce59
MaximePremont <maxime.premont@gmail.com>
2020-01-31 21:49:36 +0100

    First code commit
    Première création de la classe principale en python.

---
+++ Quoridor.py
@@ -0,0 +1,22 @@
+"""
+ Initialisation des variables
+"""
+pion1 = 0 # A définir
+pion2 = 0 # A définir
+barriers = []
+
+"""
+ Fonction principale
+"""
+def quoridor():
+    
+
+"""
+ Fonction de génération d'image
+ @select_case : Case sélectionnée
+ @pion1 : Case du pion 1
+ @pion2 : Case du pion 2
+ @barriers : Liste des barrières
+"""
+def gen_image(select_case, pions, barriers):
+    

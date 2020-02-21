4988c8d078a2e63c0dc44328b01600d0408061cf
kikisol38 <60509137+kikisol38@users.noreply.github.com>
2020-02-21 09:47:12 +0100

    Img gen
    Génération complète de l'image

--- Quoridor.py
+++ Quoridor.py
@@ -1,19 +1,17 @@
-"""
- Initialisation des variables
-"""
-cases = []
-pion1 = []
-pion2 = []
-barrieres = []
-
 
 """
  Fonction principale
 """
 def quoridor():
-    cases = creer_matrice(17, 17)
-    pion1 = [16,8]
-    pion2 = [0,8]
+    cases = creer_matrice(9, 9)
+    barrieres_verticales = creer_matrice(8, 9)
+    barrieres_horizontales = creer_matrice(9, 8)
+    pion1 = [0,4]
+    pion2 = [8,4]
+    select_case = 0
+    barrieres_verticales[1][0] = 1
+    barrieres_horizontales[6][2] = 1
+    creer_image(generer_image(select_case, pion1, pion2, barrieres_verticales, barrieres_horizontales))
 
 """
  Vérifie la présence d'un pion sur une case
@@ -34,26 +32,80 @@ def pion_present(case, pion):
 
 
 """
- Vérifie la présence d'une barrière sur telle case
- @case : Case à vérifier
-"""
-def barriere_presente(case):
-    presente = False
-    for i in range(len(barrieres)):
-        if(barrieres[i] == case):
-            presente = True
-    return presente
-
-
-"""
  Fonction de génération d'image
  @select_case : Case sélectionnée
  @pion1 : Case du pion 1
  @pion2 : Case du pion 2
- @barrieres : Liste des barrières
+ @barrieres_verticales : Liste des barrières verticales
+ @barrieres_horizontales : Liste des barrières horizontales
 """
-def generer_image(select_case, pion1, pion2, barrieres):
-    #Suite
+def generer_image(select_case, pion1, pion2, barrieres_verticales, barrieres_horizontales):
+    pixels = creer_matrice(69,69)
+    for a in range (9):
+        for b in range (9):
+            start = [8*a,8*b]
+            for c in range(5):
+                for d in range(5):
+                    pixels[start[0]+c][start[1]+d] = "N"
+            if(select_case == [a,b]):
+                for e in range (5):
+                    pixels[start[0]+e][start[1]] = "O"
+                    pixels[start[0]+e][start[1]+4] = "O"
+                for f in range (5):
+                    pixels[start[0]][start[1]+f] = "O"
+                    pixels[start[0]+4][start[1]+f] = "O"
+            if(pion1 == [a,b]):
+                for g in range(3):
+                    pixels[start[0]+2][start[1]+1+g] = "R"
+                for h in range(3):
+                    pixels[start[0]+1+h][start[1]+2] = "R"
+            elif(pion2 == [a,b]):
+                for i in range(3):
+                    pixels[start[0]+2][start[1]+1+i] = "B"
+                for j in range(3):
+                    pixels[start[0]+1+j][start[1]+2] = "B"
+    for k in range (8):
+        for l in range (8):
+            if(barrieres_verticales[k][l] == 1):
+                start_b = [k*8,l*8+5]
+                for m in range (3):
+                    for n in range(13):
+                        pixels[start_b[0]+n][start_b[1]+m] = "V"
+    for o in range (8):
+        for p in range (8):
+            if(barrieres_horizontales[o][p] == 1):
+                start_b = [o*8+5,p*8]
+                for q in range (3):
+                    for r in range(13):
+                        pixels[start_b[0]+q][start_b[1]+r] = "V"
+    return pixels
+
+"""
+ Fonction de création de l'image
+ @pixels : Matrice contenant les pixels
+"""
+def creer_image(pixels):
+    fichier=open("plateau.ppm","w")
+    fichier.write("P3\n69 69\n255\n");
+    for a in range(69):
+        ligne = ""
+        for b in range(69):
+            couleur = pixels[a][b]
+            if(couleur == "N"):
+                ligne = ligne+"0 0 0 "
+            elif(couleur == "O"):
+                ligne = ligne+"255 125 0 "
+            elif(couleur == "R"):
+                ligne = ligne+"255 0 0 "
+            elif(couleur == "B"):
+                ligne = ligne+"0 0 255 "
+            elif(couleur == "V"):
+                ligne = ligne+"80 255 0 "
+            else:
+                ligne = ligne+"255 255 255 "
+        ligne = ligne+"\n"
+        fichier.write(ligne)
+    fichier.close()
 
 
 """
@@ -63,7 +115,8 @@ def generer_image(select_case, pion1, pion2, barrieres):
 """
 def creer_matrice(lignes, colonnes):
     tableau = []
-    for i in range (colonnes):
-        ligne = [0]*lignes
-        tableau.append(ligne)
+    for i in range (lignes):
+        colonne = [0]*colonnes
+        tableau.append(colonne)
     return tableau
+

5413833e78e70f1cc73e85d4b6c84a1a8f68e7fa
kikisol38 <60509137+kikisol38@users.noreply.github.com>
2020-03-24 15:20:05 +0100

    travail clavier
    Ajout fonction clavier

--- Commandes.py
+++ Commandes.py
@@ -1,5 +1,60 @@
 from tkinter import*
 from Fenetre import*
+from Outils import*
+
+clavier = Tk()
+
+def droite(event):
+    if (selectionType == pion):
+        if(pion_present == True ):
+            print( "erreur")
+        else:
+            select_case = [0,0+1]
+            #print("Pour confirmer appuyez sur entrer")
+    else :
+        if(pion_present == True ):
+            print( "erreur")
+        else:
+            barrieres_verticales = [0,8]
+            
+def gauche(event):
+    if (selectionType == pion):
+        if(pion_present == True):
+            print( "erreur")
+        else:
+            select_case = [0,4-1]
+            #print("Pour confirmer appuyez sur entrer")
+    else :
+        if(pion_present == True): 
+            print( "erreur")
+        else:
+            barrieres_verticales = [0,8]
+def bas(event):
+    if (selectionType == pion):
+        if(pion_present == True ):
+            print( "erreur")
+        else:
+            select_case = [0+1,0]
+            #print("Pour confirmer appuyez sur entrer")
+    else :
+        if(pion_present == True ):
+            print( "erreur")
+        else:
+            barrieres_verticales = [0+1,8]
+def haut(event):
+    if(selectionType == pion):
+        if(pion_present == True):
+            print( "erreur")
+        else:
+            if(select_case == [0-1,0]):
+                print("erreur")
+            else:
+                select_case = [1-1,0]
+    else:
+        if(pion_present == True):
+            print( "erreur")
+        else:
+            barrieres_verticales = [0,8+1]
 
 """
  Ecoute l'entrée clavier
@@ -7,4 +62,8 @@ from Fenetre import*
  @selectionType : Type de sélection ( pion ou barrière )
 """
 def ecouteClavier(fenetre, selectionType):
-    
+    clavier.bind('<Right>',droite)
+    clavier.bind('<Left>',gauche)
+    clavier.bind('<Down>',bas)
+    clavier.bind('<Up>',haut)
+    clavier.bind('<Return>',entrer)
